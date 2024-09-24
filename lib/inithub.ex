1e5c2d60cd86f9f969e057ad9c8dc2fbd7b8436d
wedo <costadefelipe@gmail.com>
2024-09-24 13:27:47 -0300

    first commit

---
+++ lib/inithub.ex
@@ -0,0 +1,18 @@
+defmodule Inithub do
+  @moduledoc """
+  Documentation for `Inithub`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Inithub.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/inithub_test.exs
@@ -0,0 +1,8 @@
+defmodule InithubTest do
+  use ExUnit.Case
+  doctest Inithub
+
+  test "greets the world" do
+    assert Inithub.hello() == :world
+  end
+end
